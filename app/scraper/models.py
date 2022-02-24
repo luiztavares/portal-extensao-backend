816d933a65ede906ea22cd426dcaa29e2dff3f1d
Luiz André Tavares <luizandre.tavares@outlook.com>
2022-02-24 12:53:45 -0300

    scraper de cursos, eventos e projetos

--- app/scraper/models.py
+++ app/scraper/models.py
@@ -1,115 +1,94 @@
 from django.db import models
 
-class InstituicoesParceiras:
-    def __init__(self):
-        nome = None
-        tipo_de_parceiro = None
-        forma_de_institucionalizacao = None
-        email = None
-        endereco = None
-        tipo_de_instituicao = None
+class InstituicaosParceira(models.Model):
+    nome = models.CharField(max_length=100)
+    tipo_de_parceiro = models.CharField(max_length=100)
+    forma_de_institucionalizacao =  models.CharField(max_length=100)
+    email =  models.CharField(max_length=100)
+    endereco =  models.CharField(max_length=1000)
+    tipo_de_instituicao =  models.CharField(max_length=100)
 
-class EscolasParticipantes():
-    def __init__(self):
-        nome = None
-        tipo_de_escola = None
-        email = None
-        endereco = None
+class EscolaParticipante(models.Model):
+    nome = models.CharField(max_length=100)
+    tipo_de_escola = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=1000)
 
-class CoordenadoresAdjuntos():
-    def __init__(self):
-        nome = None
-        unidade = None
-        centro = None
+class CoordenadoresAdjuntos(models.Model):
+    nome = models.CharField(max_length=100)
+    unidade = models.CharField(max_length=100)
+    centro = models.CharField(max_length=100)
 
-class CaracterizacaoDeProjetos:
-    def __init__(self):
-        metodologia = None
-        duracao_pretendida_do_projeto = None
+class CaracterizacaoDeProjeto(models.Model):
+    metodologia = models.CharField(max_length=10000)
+    duracao_pretendida_do_projeto = models.IntegerField(null=True)
 
-class CaracterizacaoDeProgramas:
-    def __init__(self):
-        coordenadores_adjuntos = None
-        acoes_de_extensao_vinculadas = None
-        linhas_de_extensao = None
+class CaracterizacaoDePrograma(models.Model):
+    coordenadores_adjuntos = models.ManyToManyField(CoordenadoresAdjuntos)
+    acoes_de_extensao_vinculadas = models.ManyToManyField('Acao')
+    linhas_de_extensao = 'todo'
 
-class CaracterizacaoDeCursos:
-    def __init__(self):
-        carga_horaria = None
-        modalidade_do_curso = None
-        classificacao_do_curso = None
-        programa_de_formacao = None
-        conteudo_programatico_do_curso = None
-        periodos_de_oferta_do_curso_nos_proximos_5_anos = None
+class CaracterizacaoDeCurso(models.Model):
+    carga_horaria = models.IntegerField(null=True)
+    modalidade_do_curso = models.CharField(max_length=100)
+    classificacao_do_curso = models.CharField(max_length=100)
+    programa_de_formacao = models.CharField(max_length=1000)
+    conteudo_programatico_do_curso = models.CharField(max_length=1000)
+    periodos_de_oferta_do_curso_nos_proximos_5_anos = models.IntegerField(null=True)
 
-class CaracterizacaoDeEventos:
-    def __init__(self):
-        duracao_do_evento_em_numero_de_dias = None
-        quantitativo_de_publico = None
-        numero_da_edicao_do_evento = None
-        tema_geral_do_evento = None
+class CaracterizacaoDeEvento(models.Model):
+    duracao_do_evento_em_numero_de_dias = models.IntegerField(null=True)
+    quantitativo_de_publico = models.IntegerField(null=True)
+    numero_da_edicao_do_evento = models.IntegerField(null=True)
+    tema_geral_do_evento = models.CharField(max_length=100)
 
-class EquipeDeRealizacao:
-    def __init__(self):
-        nome = None
-        periodo_de_participacao = Periodo()
-        tipo = None
+class Periodo(models.Model):
+    inicio = models.DateField()
+    fim = models.DateField(null=True)
 
-class Periodo:
-    def __init__(self):
-        inicio = None
-        fim = None
+    def __str__(self):
+        return f'inicio: {self.inicio}, fim:{self.fim}'
 
-class InformacoesDaAcaoDeExtensao:
-    def __init__(self):
-        centro = None
-        unidade = None
-        titulo = None
-        modalidade = None
-        area_tematica_principal = None
-        area_tematica_secundaria = None
-        objetivos_de_desenvolvimento_sustentavel = None
-        coordenador_da_acao_de_extensao = None
-        Resumo = None
-        Contato = None
-        data_de_inicio = None
-        data_de_termino = None
-        objetivos = None
-        links_para_redes_sociaus_e_sites = None
-        endereco = None
-        publico_da_acao = None
-        carga_horaria = None
-        instituicoes_parceiras = None
-        equipe_de_realizacao = None
+class Membro(models.Model):
+    nome = models.CharField(max_length=100)
+    periodo_de_participacao = models.ForeignKey(Periodo,on_delete=models.PROTECT)
+    tipo = models.CharField(max_length=100)
 
-class Projeto:
-    def __init__():
-        informacoes_da_acao_de_extensao = InformacoesDaAcaoDeExtensao()
-        instituicoes_parceiras = InstituicoesParceiras()
-        escolas_participantes = EscolasParticipantes()
-        caracterizacao_de_projetos = CaracterizacaoDeProjetos()
-        equipe_de_realizacao = EquipeDeRealizacao()
+class InformacoesDaAcaoDeExtensao(models.Model):
+    id = models.CharField(max_length=100,primary_key=True)
+    centro = models.CharField(max_length=100)
+    unidade = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=1000)
+    modalidade = models.CharField(max_length=100)
+    area_tematica_principal = models.CharField(max_length=100)
+    area_tematica_secundaria = models.CharField(max_length=100)
+    objetivos_de_desenvolvimento_sustentavel = models.CharField(max_length=100)
+    coordenador_da_acao_de_extensao = models.CharField(max_length=100)
+    Resumo = models.CharField(max_length=1000)
+    Contato = models.CharField(max_length=100)
+    data_de_inicio = models.DateField(null=True)
+    data_de_termino = models.DateField(null=True)
+    objetivos = models.CharField(max_length=1000)
+    links_para_redes_sociaus_e_sites = models.CharField(max_length=1000)
+    endereco = models.CharField(max_length=1000)
+    publico_da_acao = models.CharField(max_length=100)
+    carga_horaria = models.IntegerField(null=True)
 
-class Programa:
-    def __init__():
-        informacoes_da_acao_de_extensao = InformacoesDaAcaoDeExtensao()
-        instituicoes_parceiras = InstituicoesParceiras()
-        escolas_participantes = EscolasParticipantes()
-        caracterizacao_de_programas = CaracterizacaoDeProgramas()
-        equipe_de_realizacao = EquipeDeRealizacao()
+class Acao(models.Model):
+    id = models.CharField(max_length=100,primary_key=True)
+    informacoes_da_acao_de_extensao = models.ForeignKey(InformacoesDaAcaoDeExtensao,on_delete=models.PROTECT)
+    instituicoes_parceiras = models.ManyToManyField(InstituicaosParceira)
+    escolas_participantes = models.ManyToManyField(EscolaParticipante)
+    equipe_de_realizacao = models.ManyToManyField(Membro)
 
-class Curso:
-    def __init__():
-        informacoes_da_acao_de_extensao = InformacoesDaAcaoDeExtensao()
-        instituicoes_parceiras = InstituicoesParceiras()
-        escolas_participantes = EscolasParticipantes()
-        caracterizacao_de_cursos = CaracterizacaoDeCursos()
-        equipe_de_realizacao = EquipeDeRealizacao()
+class Projeto(Acao):
+    caracterizacao_de_projetos = models.ForeignKey(CaracterizacaoDeProjeto,on_delete=models.PROTECT)
 
-class Evento:
-    def __init__():
-        informacoes_da_acao_de_extensao = InformacoesDaAcaoDeExtensao()
-        instituicoes_parceiras = InstituicoesParceiras()
-        escolas_participantes = EscolasParticipantes()
-        caracterizacao_de_eventos = CaracterizacaoDeEventos()
-        equipe_de_realizacao = EquipeDeRealizacao()
+class Programa(Acao):
+    caracterizacao_de_programas = models.ForeignKey(CaracterizacaoDePrograma,on_delete=models.PROTECT)
+
+class Curso(Acao):
+    caracterizacao_de_cursos = models.ForeignKey(CaracterizacaoDeCurso,on_delete=models.PROTECT)
+
+class Evento(Acao):
+    caracterizacao_de_eventos = models.ForeignKey(CaracterizacaoDeEvento,on_delete=models.PROTECT)
--- app/scraper/tests.py
+++ app/scraper/tests.py
@@ -1,4 +1,5 @@
 from ast import Eq
+from tkinter import N
 from django.test import TestCase
 import time
 from scraper.models import *
@@ -9,9 +10,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
+modalidades = {
+    #0: 'Curso',
+    #1: 'Evento',
+    #2: 'Programa',
+    3: 'Projeto',
+}
 
+def toDate(s):
+    s = s.replace(' ','')
 
-def init_driver():
+    if (len(s) >= 10):
+        return f'{s[6:10]}-{s[3:5]}-{s[:2]}'
+    else:
+        return None
+
+def num(n):
+    return None if n == '-' else n
+
+def init_driver(modalidade):
 
     chrome_options = Options()  
     chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
@@ -19,17 +36,137 @@ def init_driver():
     driver = webdriver.Chrome(executable_path='/opt/homebrew/bin/chromedriver', chrome_options=chrome_options)
 
     driver.get("https://portal.ufrj.br/Inscricao/extensao/acaoExtensao/filtro")
+    driver.find_element_by_xpath(f"//select[@id='modalidade']//option[text()='{modalidade}']").click()
+    botao_consulta = driver.find_element_by_id("btnConsultar")
+    botao_consulta.click()
+
     return driver
 
-driver = init_driver()
+for key in modalidades:
 
-botao_consulta = driver.find_element_by_id("btnConsultar")
-botao_consulta.click()
+    driver = init_driver(modalidades[key])
 
-driver.find_element_by_css_selector('.rich-table-cell.lupa a').click()
+    num_paginas = int(driver.find_element_by_css_selector('#gnosys-filtro-paginacao-campos span').text)
+    for pag_atual in range(1,num_paginas+1) :
+        atual = driver.find_element_by_css_selector('#gnosys-filtro-paginacao-paginaAtual')
+        atual.clear()
+        atual.send_keys(pag_atual)
+        atual.send_keys(Keys.ENTER)
+        time.sleep(2)
+        page_links = driver.find_elements_by_css_selector('.rich-table-cell.lupa a')
 
+        for i in range(len(page_links)):
+            atual = driver.find_element_by_css_selector('#gnosys-filtro-paginacao-paginaAtual')
+            atual.clear()
+            atual.send_keys(pag_atual)
+            atual.send_keys(Keys.ENTER)
+            time.sleep(2)
+            print(pag_atual,i)
+            driver.find_elements_by_css_selector('.rich-table-cell.lupa a')[i].click()
 
-centro = driver.find_element_by_css_selector('#centroNomeDecorate span').text
-print(centro)
+            acao = None
+            id = driver.current_url.split('?id=')[1].split('&')[0]
+            carga_horaria = driver.find_element_by_css_selector('#cargaHorariaDecorate span').text
+            informacoesDaAcaoDeExtensao = InformacoesDaAcaoDeExtensao(
+                id = id,
+                centro = driver.find_element_by_css_selector('#centroNomeDecorate span').text,
+                unidade = driver.find_element_by_css_selector('#unidadeNomeDecorate span').text,
+                titulo = driver.find_element_by_css_selector('#acaoExtensaoNomeDecorate span').text,
+                modalidade = driver.find_element_by_css_selector('#acaoExtensaoModalidadeDecorate span').text,
+                area_tematica_principal = driver.find_element_by_css_selector('#areaTematicaPrimariaExtensaoNomeDecorate span').text,
+                area_tematica_secundaria = driver.find_element_by_css_selector('#areaTematicaSecundariaExtensaoNomeDecorate span').text,
+                objetivos_de_desenvolvimento_sustentavel = 'todo',
+                coordenador_da_acao_de_extensao = driver.find_element_by_css_selector('#coordenadorNomeDecorate span').text,
+                Resumo = driver.find_element_by_css_selector('#descricaoAcaoExtensaoDecorate span').text,
+                Contato = driver.find_element_by_css_selector('#contatoDecorate span').text,
+                data_de_inicio = toDate(driver.find_element_by_css_selector('#dataInicioDecorate span').text),
+                data_de_termino = toDate(driver.find_element_by_css_selector('#dataFimDecorate span').text),
+                objetivos = driver.find_element_by_css_selector('#objetivosDecorate span').text,
+                links_para_redes_sociaus_e_sites = driver.find_element_by_css_selector('#linksAcaoExtensaoDecorate span').text,
+                endereco = driver.find_element_by_css_selector('#enderecosDecorate span').text,
+                publico_da_acao = driver.find_element_by_css_selector('#publicoAcaoDecorate span').text,
+                carga_horaria = num(driver.find_element_by_css_selector('#cargaHorariaDecorate span').text),
+            )
+            informacoesDaAcaoDeExtensao.save()
 
-driver.close()
+            if (informacoesDaAcaoDeExtensao.modalidade == 'Curso'):
+                caracterizacaoDeCurso = CaracterizacaoDeCurso(
+                    carga_horaria = num(driver.find_element_by_css_selector('#cargaHorariaTotalDecorate span').text),
+                    modalidade_do_curso = driver.find_element_by_css_selector('#modalidadeCursoDecorate span').text,
+                    classificacao_do_curso = driver.find_element_by_css_selector('#classificacaoCursoDecorate span').text,
+                    programa_de_formacao = driver.find_element_by_css_selector('#programaFormacaoDecorate span').text,
+                    conteudo_programatico_do_curso = driver.find_element_by_css_selector('#conteudoProgramaticoDecorate span').text,
+                    periodos_de_oferta_do_curso_nos_proximos_5_anos = '0'
+                )
+                caracterizacaoDeCurso.save()
+                acao = Curso(id=id,informacoes_da_acao_de_extensao=informacoesDaAcaoDeExtensao,caracterizacao_de_cursos=caracterizacaoDeCurso)
+
+            elif(informacoesDaAcaoDeExtensao.modalidade == 'Projeto'):
+                caracterizacaoDeProjeto = CaracterizacaoDeProjeto(
+                    metodologia = driver.find_element_by_css_selector('#metodologiaDecorate span').text,
+                    duracao_pretendida_do_projeto = num(driver.find_element_by_css_selector('#duracaoPretendidaDecorate span').text),
+                )
+                caracterizacaoDeProjeto.save()
+                acao = Projeto(id=id,informacoes_da_acao_de_extensao=informacoesDaAcaoDeExtensao,caracterizacao_de_projetos=caracterizacaoDeProjeto)
+
+            elif(informacoesDaAcaoDeExtensao.modalidade == 'Evento'):
+                caracterizacaoDeEvento = CaracterizacaoDeEvento(
+                    duracao_do_evento_em_numero_de_dias = num(driver.find_element_by_css_selector('#duracaoDiasDecorate span').text),
+                    quantitativo_de_publico = num(driver.find_element_by_css_selector('#publicoEsperadoDecorate span').text),
+                    numero_da_edicao_do_evento = num(driver.find_element_by_css_selector('#numeroEdicaoDecorate span').text),
+                    tema_geral_do_evento = driver.find_element_by_css_selector('#temaGeralEventoDecorate span').text,
+                )
+                caracterizacaoDeEvento.save()
+                acao = Evento(id=id,informacoes_da_acao_de_extensao=informacoesDaAcaoDeExtensao,caracterizacao_de_eventos=caracterizacaoDeEvento)
+
+            acao.save()
+            tipos_de_instituicao = ['Nacionais','Internacionais']
+            for tipo_de_instituicao in tipos_de_instituicao:
+                parceiros_nacionais_element = driver.find_elements_by_css_selector(f'#tabelaInstituicoesParceiras{tipo_de_instituicao} tbody tr')
+                for parceiro_nacional_element in parceiros_nacionais_element:
+                    colunas = parceiro_nacional_element.find_elements_by_css_selector('td')
+                    instituicaosParceira = InstituicaosParceira(
+                        nome = colunas[0].text,
+                        tipo_de_parceiro = colunas[1].find_element_by_css_selector('span').text,
+                        forma_de_institucionalizacao = colunas[2].find_element_by_css_selector('span').text,
+                        email = colunas[3].text,
+                        endereco = colunas[4].text,
+                        tipo_de_instituicao = tipo_de_instituicao.lower(),
+                    )
+                    instituicaosParceira.save()
+                    acao.instituicoes_parceiras.add(instituicaosParceira)
+
+            escolas_element = driver.find_elements_by_css_selector(f'#tabelaEscolasParticipantes tbody tr')
+            for escola_element in escolas_element:
+                colunas = escola_element.find_elements_by_css_selector('td')
+                escolaParticipante = EscolaParticipante(
+                    nome = colunas[0].text,
+                    tipo_de_escola = colunas[1].text,
+                    email = colunas[2].text,
+                    endereco = colunas[3].text,
+                )
+                escolaParticipante.save()
+                acao.escolas_participantes.add(escolaParticipante)
+
+            tipos_de_membro = {
+                'interno': 'tabelaEquipeDeRealizacaoUFRJ',
+                'externo' : 'tabelaEquipeDeRealizacaoExterno',
+                'extensionista': 'tabelaAlunosExtensao',
+            }
+            for tipo_de_membro in tipos_de_membro:
+                membros_element = driver.find_elements_by_css_selector(f'#{tipos_de_membro[tipo_de_membro]} tbody tr')
+                for membro_element in membros_element:
+                    colunas = membro_element.find_elements_by_css_selector('td')
+                    inicio,fim = colunas[1].find_element_by_css_selector('span').text.split('-')
+                    periodo = Periodo(inicio = toDate(inicio), fim=toDate(fim))
+                    periodo.save()
+                    membro = Membro(
+                        nome = colunas[0].text,
+                        periodo_de_participacao = periodo,
+                        tipo = tipo_de_membro,
+                    )
+                    membro.save()
+                    acao.equipe_de_realizacao.add(membro)
+
+            driver.execute_script("window.history.go(-1)")
+    driver.close()
